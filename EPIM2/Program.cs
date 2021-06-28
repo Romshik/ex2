4576811108ed3de16286d6e9955d618bd7ee4d1c
37525 <37525@DESKTOP-RLKC77D>
2021-06-28 17:55:37 +0300

    Добавьте файлы проекта.

---
+++ EPIM2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+using System.Text;
+
+
+namespace EPIM2
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger res;
+
+            try
+            {
+
+                Console.WriteLine("First numb: ");
+                BigInteger a = BigInteger.Parse(Console.ReadLine());
+                Console.WriteLine("Action (*, /, +, -) ");
+                char z = Convert.ToChar(Console.ReadLine());
+             
+                if (z == '+' || z == '-' || z == '/' || z == '*')
+                {
+                    Console.WriteLine("Second numb: ");
+                    BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+                    if (z == '+')
+                    {
+                        res = a + b;
+                        Console.WriteLine("Res: " + res);
+                    }
+                    if (z == '-')
+                    {
+                        res = a - b;
+                        Console.WriteLine("Res: " + res);
+                    }
+
+                    if (z == '*')
+                    {
+                        res = a * b;
+                        Console.WriteLine("Res: " + res);
+                    }
+                    if (z == '/')
+                    {
+                        res = a / b;
+                        Console.WriteLine("Res: " + res);
+                    }
+
+                }
+                else { Console.WriteLine("Wrong action");
+                    
+                }
+            }
+            catch { Console.WriteLine("Error!");}
+        }
+    }
+}
